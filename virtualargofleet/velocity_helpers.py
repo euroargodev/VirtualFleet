31dd28ca64de12445d5586c5bfe78d85b7bdb8fd
Guillaume Maze <gmaze@ifremer.fr>
2022-10-10 16:59:57 +0200

    Update velocity_helpers.py
    Add MEDSEA_ANALYSISFORECAST_PHY_006_013 as known dataset

--- virtualargofleet/velocity_helpers.py
+++ virtualargofleet/velocity_helpers.py
@@ -1,19 +1,17 @@
 """
 Velocity Field Helper
-
-
 """
 
 from parcels import FieldSet, ParticleSet, Field
 import xarray as xr
 import glob
-from abc import ABC, abstractmethod
+from abc import ABC
 
 from .app_parcels import ArgoParticle
 
 
 def VelocityFieldFacade(model: str = 'GLOBAL_ANALYSIS_FORECAST_PHY_001_024', *args, **kwargs):
-    """Thin layer above Parcels to manage velocity fields definition.
+    """Thin layer above Parcels to manage velocity fields definition for known products
 
     .. warning::
 
@@ -34,6 +32,8 @@ def VelocityFieldFacade(model: str = 'GLOBAL_ANALYSIS_FORECAST_PHY_001_024', *ar
     """
     if model in ['GLOBAL_ANALYSIS_FORECAST_PHY_001_024', 'PSY4QV3R1', 'GLORYS12V1']:
         return VelocityField_GLOBAL_ANALYSIS_FORECAST_PHY_001_024(*args, **kwargs)
+    elif model in ['MEDSEA_ANALYSISFORECAST_PHY_006_013']:
+        return VelocityField_MEDSEA_ANALYSISFORECAST_PHY_006_013(*args, **kwargs)
     else:
         raise ValueError('Unknown model')
 
@@ -119,5 +119,69 @@ class VelocityField_GLOBAL_ANALYSIS_FORECAST_PHY_001_024(VelocityFieldProto):
 
     def __repr__(self):
         summary = ["<VelocityField.GLOBAL_ANALYSIS_FORECAST_PHY_001_024>"]
+
+        return "\n".join(summary)
+
+
+class VelocityField_MEDSEA_ANALYSISFORECAST_PHY_006_013(VelocityFieldProto):
+    """Velocity Field Helper for CMEMS/MEDSEA_ANALYSISFORECAST_PHY_006_013 product.
+
+    Reference
+    ---------
+    https://resources.marine.copernicus.eu/product-detail/MEDSEA_ANALYSISFORECAST_PHY_006_013/DATA-ACCESS
+
+    """
+
+    def __init__(self, src, **kwargs):
+        """
+
+        Parameters
+        ----------
+        src: pattern
+            Pattern to list netcdf source files
+        isglobal : bool, default False
+            Set to 1 if field is global, 0 otherwise
+        """
+        filenames = {'U': src, 'V': src}
+        variables = {'U': 'uo', 'V': 'vo'}
+        dimensions = {'time': 'time', 'depth': 'depth', 'lat': 'lat', 'lon': 'lon'}
+
+        self.field = filenames  # Dictionary with 'U' and 'V' as keys and list of corresponding files as values
+        self.var = variables  # Dictionary mapping 'U' and 'V' to netcdf velocity variable names
+        self.dim = dimensions  # Dictionary mapping 'time', 'depth', 'lat' and 'lon' to netcdf velocity variable names
+
+        # define parcels fieldset
+        self.fieldset = FieldSet.from_netcdf(
+            self.field, self.var, self.dim,
+            allow_time_extrapolation=True,
+            time_periodic=False,
+            deferred_load=True)
+
+        # Create mask to manage grounding:
+        self.add_mask()
+
+    def add_mask(self):
+        """Create mask for grounding management """
+        mask_file = glob.glob(self.field['U'])[0]
+        ds = xr.open_dataset(mask_file)
+        ds = eval("ds.isel("+self.dim['time']+"=0)")
+        ds = ds[[self.var['U'], self.var['V']]].squeeze()
+
+        mask = ~(ds.where((~ds[self.var['U']].isnull()) | (~ds[self.var['V']].isnull()))[
+                 self.var['U']].isnull()).transpose(self.dim['lon'], self.dim['lat'], self.dim['depth'])
+        mask = mask.values
+
+        # create a new parcels field that's going to be interpolated during simulation
+        self.fieldset.add_field(Field('mask',
+                                      data=mask,
+                                      lon=ds[self.dim['lon']].values,
+                                      lat=ds[self.dim['lat']].values,
+                                      depth=ds[self.dim['depth']].values,
+                                      transpose=True,
+                                      mesh='spherical',
+                                      interp_method='nearest'))
+
+    def __repr__(self):
+        summary = ["<VelocityField.MEDSEA_ANALYSISFORECAST_PHY_006_013>"]
 
         return "\n".join(summary)
