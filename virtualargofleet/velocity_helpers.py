de85845a33b9a5bea8a497c002a8a9a9386a853e
Guillaume Maze <gmaze@ifremer.fr>
2022-10-17 16:13:06 +0200

    Update velocity_helpers.py
    - add velocity support for ARMOR3D

--- virtualargofleet/velocity_helpers.py
+++ virtualargofleet/velocity_helpers.py
@@ -34,6 +34,8 @@ def VelocityFieldFacade(model: str = 'GLOBAL_ANALYSIS_FORECAST_PHY_001_024', *ar
         return VelocityField_GLOBAL_ANALYSIS_FORECAST_PHY_001_024(*args, **kwargs)
     elif model in ['MEDSEA_ANALYSISFORECAST_PHY_006_013']:
         return VelocityField_MEDSEA_ANALYSISFORECAST_PHY_006_013(*args, **kwargs)
+    elif model in ['MULTIOBS_GLO_PHY_TSUV_3D_MYNRT_015_012', 'ARMOR3D']:
+        return VelocityField_MULTIOBS_GLO_PHY_TSUV_3D_MYNRT_015_012(*args, **kwargs)
     else:
         raise ValueError('Unknown model')
 
@@ -198,3 +200,93 @@ class VelocityField_MEDSEA_ANALYSISFORECAST_PHY_006_013(VelocityFieldProto):
         summary = ["<VelocityField.MEDSEA_ANALYSISFORECAST_PHY_006_013>"]
 
         return "\n".join(summary)
+
+
+class VelocityField_MULTIOBS_GLO_PHY_TSUV_3D_MYNRT_015_012(VelocityFieldProto):
+    """Velocity Field Helper for CMEMS/ARMOR3D product.
+
+    Reference
+    ---------
+    https://resources.marine.copernicus.eu/product-detail/MULTIOBS_GLO_PHY_TSUV_3D_MYNRT_015_012/DATA-ACCESS
+
+    """
+
+    def __init__(self, src, isglobal: bool = False, **kwargs):
+        """
+
+        Parameters
+        ----------
+        src: pattern
+            Pattern to list netcdf source files
+        isglobal : bool, default False
+            Set to 1 if field is global, 0 otherwise
+        """
+        variables = {'U': 'ugo', 'V': 'vgo'}
+        dimensions = {'time': 'time', 'depth': 'depth', 'lat': 'latitude', 'lon': 'longitude'}
+        if not isinstance(src, xr.core.dataset.Dataset):
+            filenames = {'U': src, 'V': src}
+            self.field = filenames  # Dictionary with 'U' and 'V' as keys and list of corresponding files as values
+        else:
+            self.field = src  # Xarray dataset
+
+        self.var = variables  # Dictionary mapping 'U' and 'V' to netcdf velocity variable names
+        self.dim = dimensions  # Dictionary mapping 'time', 'depth', 'lat' and 'lon' to netcdf velocity variable names
+        self.isglobal = isglobal
+
+        # define parcels fieldset
+        if not isinstance(src, xr.core.dataset.Dataset):
+            self.fieldset = FieldSet.from_netcdf(
+                self.field, self.var, self.dim,
+                allow_time_extrapolation=True,
+                time_periodic=False,
+                deferred_load=True)
+        else:
+            self.fieldset = FieldSet.from_xarray_dataset(
+                self.field, self.var, self.dim,
+                allow_time_extrapolation=True,
+                time_periodic=False)
+
+        if self.isglobal:
+            self.fieldset.add_constant(
+                'halo_west', self.fieldset.U.grid.lon[0])
+            self.fieldset.add_constant(
+                'halo_east', self.fieldset.U.grid.lon[-1])
+            self.fieldset.add_constant(
+                'halo_south', self.fieldset.U.grid.lat[0])
+            self.fieldset.add_constant(
+                'halo_north', self.fieldset.U.grid.lat[-1])
+            self.fieldset.add_periodic_halo(zonal=True, meridional=True)
+
+        # Create mask to manage grounding:
+        self.add_mask()
+
+    def add_mask(self):
+        """Create mask for grounding management """
+        if not isinstance(self.field, xr.core.dataset.Dataset):
+            mask_file = glob.glob(self.field['U'])[0]
+            ds = xr.open_dataset(mask_file)
+            ds = eval("ds.isel("+self.dim['time']+"=0)")
+            ds = ds[[self.var['U'], self.var['V']]].squeeze()
+        else:
+            ds = eval("self.field.isel("+self.dim['time']+"=0)")
+            ds = ds[[self.var['U'], self.var['V']]].squeeze()
+
+        mask = ~(ds.where((~ds[self.var['U']].isnull()) | (~ds[self.var['V']].isnull()))[
+                 self.var['U']].isnull()).transpose(self.dim['lon'], self.dim['lat'], self.dim['depth'])
+        mask = mask.values
+
+        # create a new parcels field that's going to be interpolated during simulation
+        self.fieldset.add_field(Field('mask',
+                                      data=mask,
+                                      lon=ds[self.dim['lon']].values,
+                                      lat=ds[self.dim['lat']].values,
+                                      depth=ds[self.dim['depth']].values,
+                                      transpose=True,
+                                      mesh='spherical',
+                                      interp_method='nearest'))
+
+    def __repr__(self):
+        summary = ["<VelocityField.ARMOR3D.MULTIOBS_GLO_PHY_TSUV_3D_MYNRT_015_012>"]
+
+        return "\n".join(summary)
+
