e78118c8dbfecc019c3be77e5094401e840b996a
Guillaume Maze <gmaze@ifremer.fr>
2022-12-16 15:59:29 +0100

    Update _version.py

--- virtualargofleet/_version.py
+++ virtualargofleet/_version.py
@@ -1,7 +1,12 @@
-from importlib.metadata import version, PackageNotFoundError
 try:
-    __version__ = version("virtualfleet")
-except PackageNotFoundError:
-    # package is not installed
+    from importlib.metadata import version as _version
+except ImportError:
+    # if the fallback library is missing, we are doomed.
+    from importlib_metadata import version as _version
+
+try:
+    __version__ = _version("virtualfleet")
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
     __version__ = '999'
-    pass
