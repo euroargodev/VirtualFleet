aa4f945164aa8939830a26365a1ce7ee3f7ee9dd
Guillaume Maze <gmaze@ifremer.fr>
2022-12-16 16:05:00 +0100

    Update __init__.py
    raise a warning instead of error if parcels is too old

--- virtualargofleet/__init__.py
+++ virtualargofleet/__init__.py
@@ -4,12 +4,12 @@ from .utilities import FloatConfiguration, ConfigParam
 from .velocity_helpers import VelocityFieldFacade as VelocityField
 from ._version import __version__
 import parcels
-
+import warnings
 
 if pack_version.parse(parcels.__version__) < pack_version.parse("2.4.0"):
     msg = "You're running Parcels %s but VirtualFleet no longer support Parcels versions " \
           "lower than 2.4.0, please upgrade." % pack_version.parse(parcels.__version__),
-    raise ValueError(msg)
+    warnings.warn(msg)
 
 #
 __all__ = (
