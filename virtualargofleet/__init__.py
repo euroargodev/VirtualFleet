4d3472fd25b37b8806dfb16da5db89c53f9bc509
Guillaume Maze <gmaze@ifremer.fr>
2023-01-11 16:37:22 +0100

    Update conf for docs and pip

--- virtualargofleet/__init__.py
+++ virtualargofleet/__init__.py
@@ -1,15 +1,16 @@
-from packaging import version as pack_version
 from .virtualargofleet import VirtualFleet
 from .utilities import FloatConfiguration, ConfigParam
 from .velocity_helpers import VelocityFieldFacade as VelocityField
+
 import parcels
 import warnings
-
+from packaging.version import parse as parse_version
 try:
     from importlib.metadata import version as il_version
 except ImportError:
     # if the fallback library is missing, we are doomed.
     from importlib_metadata import version as il_version
+
 
 try:
     __version__ = il_version("virtualfleet")
@@ -19,9 +20,10 @@ except Exception:
     __version__ = '999'
 
 
-if pack_version.parse(parcels.__version__) < pack_version.parse("2.4.0"):
+parcels_version = parcels.__version__.split("v")[1].split(".")
+if int(parcels_version[0]) <= 2 or int(parcels_version[1]) < 4:
     msg = "You're running Parcels %s but VirtualFleet no longer support Parcels versions " \
-          "lower than 2.4.0, please upgrade." % pack_version.parse(parcels.__version__),
+          "lower than 2.4, please upgrade." % parcels.__version__,
     warnings.warn(str(msg))
 
 #
